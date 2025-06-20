4affa4ed549a35b4a24eaaec22e77e7e308a5254
intermaus1154 <amarco1154@gmail.com>
2025-06-20 12:27:09 +0100

    init repo

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ require "vendor/autoload.php";
 use Nokon\NokonScalarTool\NokonString;
 
 $str = "hello world";
-$string = NokonString::build([]);
+$string = NokonString::build($str)->toUpperCase()->get();
+echo $string;
